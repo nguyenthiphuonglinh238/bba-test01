d2f855b844d9279060bd73e690d49d6c3a643a81
test 01- linh <nguyen.thi.phuong.linh.ngong238@gmail.com>
2026-03-05 18:17:30 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,7 @@
+let powerUp = "none";
+let compare = powerUp === "mushroom" ? "Mario becomes Super" :
+              powerUp === "flower" ? "Mario can shoot fireballs!" :
+              powerUp === "star" ? "Mario is invincible!" :
+              powerUp === "none" ? "Mario is normal" :"Unknown power-up";
+console.log(compare);
+            
